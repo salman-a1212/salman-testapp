b431b2702abebd2334b2cdd448d0a3a782263e6a
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2022-03-03 10:53:57 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import "./App.css";
 import person from "./img/person.jpeg";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { React, useState } from "react";
+// import validator from "validator";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [allEntry, setAllEntry] = useState([]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const newEntry = { email: email, password: password };
+    setAllEntry([...allEntry, newEntry]);
+    console.log(allEntry);
+  };
+  // const validateEmail = (e) => {
+  //   let email = e.target.value;
+
+  //   if (validator.isEmail(email)) {
+  //     setEmailError("Valid Email :)");
+  //   } else {
+  //     setEmailError("Enter valid Email!");
+  //   }
+  // };
+
   return (
     <>
       <p style={{ textAlign: "left", padding: "0 100px" }}>
@@ -15,11 +38,23 @@ function App() {
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
             quasi!
           </p>
-          <form action='' className='form'>
+          <form action='' className='form' onSubmit={submitForm}>
             <label htmlFor='email'>Email</label>
-            <input type='email' name='' id='' />
+            <input
+              type='email'
+              name='email'
+              id='email'
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
             <label htmlFor='password'>Password</label>
-            <input type='password' name='' id='' />
+            <input
+              type='password'
+              name=''
+              id=''
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
             <button type='submit'>Create an account</button>
           </form>
           <p style={{ textAlign: "center" }}>or signup with</p>
